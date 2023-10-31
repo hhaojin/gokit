736938e08e963b60a786c5f300dda8996985093b
hhaojin <31870227+hhaojin@users.noreply.github.com>
2023-10-31 11:45:28 +0800

    opts

--- simplecache/cacheable_test.go
+++ simplecache/cacheable_test.go
@@ -10,11 +10,9 @@ import (
 
 func TestSimpleCache_MGet(t *testing.T) {
 	var opr ICacheOperator //自定义的缓存操作类，实现 ICacheOperator 接口
-	cache := New[int, int](time.Second, &CacheOpts[int]{
-		Operator: opr,
-	})
+	cache := New[int64, *UserDept]().WithOperator(opr)
 
-	ret, err := cache.MGet(context.Background(), []int{1}, func(ctx context.Context, keys []int) (map[int]int, error) {
+	ret, err := cache.MGet(context.Background(), []int{1}, func(ctx context.Context, keys []int, opts ...interface{}) (map[int]int, error) {
 		fmt.Println(1)
 		return nil, nil
 	})
@@ -24,26 +22,25 @@ func TestSimpleCache_MGet(t *testing.T) {
 func TestSimpleCache_Custom(t *testing.T) {
 
 	var opr ICacheOperator //自定义的缓存操作类，实现 ICacheOperator 接口
+	var ser ISerializer
 
-	preFn := func(keys []int) []string {
-		preKeys := make([]string, 0, len(keys))
-		for _, key := range keys {
-			preKeys = append(preKeys, fmt.Sprintf("pre_%d", key))
-		}
-		return preKeys
-	}
-	crossVal := []byte("empty")
-	cache := New[int, int](time.Second, &CacheOpts[int]{
-		EmptyCacheVal: crossVal,
-		FmtKeysFunc:   preFn,
-		Serializer:    NewJsonSerializer(),
-		Operator:      opr,
-		Policy:        NewDefaultPolicy[int](time.Second, opr, crossVal, preFn),
-	})
+	cache := New[int64, *UserDept]().
+		WithOperator(opr).
+		WithSerializer(ser).
+		WithEmptyVal([]byte("--")).
+		WithFmtKeyFn(func(keys []int64, opts ...interface{}) []string {
+			fmt.Println("FmtKeyFn", opts[0], opts[1], opts[2]) // 1 2 3
+			
+			fmtKeys := make([]string, 0, len(keys))
+			for _, key := range keys {
+				fmtKeys = append(fmtKeys, fmt.Sprintf("prefix:%v:%d", opts[0], key))
+			}
+			return fmtKeys
+		})
 
-	ret, err := cache.MGet(context.Background(), []int{1}, func(ctx context.Context, keys []int) (map[int]int, error) {
-		fmt.Println(1)
+	ret, err := cache.MGet(context.Background(), []int{1}, func(ctx context.Context, keys []int, opts ...interface{}) (map[int]int, error) {
+		fmt.Println("MGet", opts[0], opts[1], opts[2]) // 1 2 3
 		return nil, nil
-	})
+	}, 1, 2, 3)
 	fmt.Println(ret, err)
 }
