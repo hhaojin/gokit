e0785427bdd5e49262ecc25451a0af1b373b95ba
hhaojin <31870227+hhaojin@users.noreply.github.com>
2023-10-31 11:39:01 +0800

    支持可选项

--- simplecache/cacheable.go
+++ simplecache/cacheable.go
@@ -3,149 +3,121 @@ package simplecache
 import (
 	"bytes"
 	"context"
-	"fmt"
-	"time"
 )
 
 type (
-	GetterFunc[KT comparable, VT any] func(ctx context.Context, keys []KT) (result map[KT]VT, err error)
+	// GetterFunc Get方法没有命中缓存的情况会调用这个方法
+	GetterFunc[VT any] func(ctx context.Context, key string) (val VT, err error)
 
+	// MGetterFunc Met方法没有命中缓存的情况会调用这个方法
+	MGetterFunc[KT comparable, VT any] func(ctx context.Context, keys []KT, opts ...interface{}) (result map[KT]VT, err error)
+
+	// FmtKeyFunc 使用该方法给格式化key, 例如加前缀, opts 透传MGET 方法的opts参数
+	FmtKeyFunc[KT comparable] func(keys []KT, opts ...interface{}) []string
+
+	// SimpleCache 通用缓存方法，KT是key 的类型， VT是值的类型
 	SimpleCache[KT comparable, VT any] struct {
-		expire        time.Duration
-		emptyCacheVal []byte
-		fmtKeysFunc   FmtKeysFunc[KT]
 		serializer    ISerializer
 		operator      ICacheOperator
-		policy        IPolicy[KT]
-	}
-
-	CacheOpts[KT comparable] struct {
-		Operator      ICacheOperator
-		EmptyCacheVal []byte
-		FmtKeysFunc   FmtKeysFunc[KT]
-		Serializer    ISerializer
-		Policy        IPolicy[KT]
+		Getter        GetterFunc[VT]
+		emptyCacheVal []byte
+		fmtKeyFn      FmtKeyFunc[KT]
 	}
 )
 
-func New[KT comparable, VT any](expire time.Duration, opts *CacheOpts[KT]) *SimpleCache[KT, VT] {
-	if opts.Operator == nil {
-		panic("opr not be nil")
-	}
+const defaultEmptyCacheVal = "-"
+
+func New[KT comparable, VT any]() *SimpleCache[KT, VT] {
 	c := &SimpleCache[KT, VT]{
-		expire:        expire,
-		emptyCacheVal: []byte("-"),
-		operator:      opts.Operator,
 		serializer:    NewJsonSerializer(),
-		policy: NewDefaultPolicy[KT](expire, opts.Operator, []byte("-"), func(keys []KT) []string {
-			preKeys := make([]string, 0, len(keys))
-			for _, key := range keys {
-				preKeys = append(preKeys, fmt.Sprintf("%v", key))
-			}
-			return preKeys
-		}),
-	}
-	if opts.EmptyCacheVal != nil {
-		c.emptyCacheVal = opts.EmptyCacheVal
-	}
-	if opts.FmtKeysFunc != nil {
-		c.fmtKeysFunc = opts.FmtKeysFunc
-		c.policy = NewDefaultPolicy[KT](expire, c.operator, c.emptyCacheVal, c.fmtKeysFunc)
-	}
-	if opts.Policy != nil {
-		c.policy = opts.Policy
-	}
-	if opts.Serializer == nil {
-		c.serializer = opts.Serializer
+		emptyCacheVal: []byte(defaultEmptyCacheVal),
 	}
 	return c
 }
 
-func (c *SimpleCache[KT, VT]) MGet(ctx context.Context, keys []KT, getter GetterFunc[KT, VT]) (result map[KT]VT, err error) {
-	var unHitKeys []KT
-	result, unHitKeys, err = c.getCache(ctx, keys)
-	if err != nil {
-		return
-	}
-	err = c.getterSetCache(ctx, unHitKeys, func(ctx context.Context, unHitKeys []KT) (map[KT]VT, error) {
-		getterResult, err := getter(ctx, unHitKeys)
-		if err != nil {
-			return nil, err
-		}
-		for kt, vt := range getterResult {
-			result[kt] = vt
-		}
-		return getterResult, nil
-	})
-	return
+func (c *SimpleCache[KT, VT]) WithOperator(o ICacheOperator) *SimpleCache[KT, VT] {
+	c.operator = o
+	return c
 }
 
-func (c *SimpleCache[KT, VT]) getCache(ctx context.Context, keys []KT) (map[KT]VT, []KT, error) {
-	var (
-		cacheResult [][]byte
-		err         error
-	)
-	preKeys, err := c.policy.Pre(keys)
-	if err != nil {
-		return nil, nil, err
-	}
+func (c *SimpleCache[KT, VT]) WithSerializer(s ISerializer) *SimpleCache[KT, VT] {
+	c.serializer = s
+	return c
+}
+
+func (c *SimpleCache[KT, VT]) WithFmtKeyFn(prefixFn FmtKeyFunc[KT]) *SimpleCache[KT, VT] {
+	c.fmtKeyFn = prefixFn
+	return c
+}
+
+func (c *SimpleCache[KT, VT]) WithEmptyVal(val []byte) *SimpleCache[KT, VT] {
+	c.emptyCacheVal = val
+	return c
+}
+
+func (c *SimpleCache[KT, VT]) MGet(ctx context.Context, keys []KT, getter MGetterFunc[KT, VT], opts ...interface{}) (result map[KT]VT, err error) {
+	var cacheResult [][]byte
+	preKeys := c.fmtKeyFn(keys, opts...)
 	if cacheResult, err = c.operator.MGet(preKeys); err != nil {
-		return nil, nil, err
+		return
 	}
-	result := make(map[KT]VT, len(cacheResult))
-	if len(cacheResult) == 0 {
-		return result, keys, nil
-	}
+	result = make(map[KT]VT)
 	unHitKeys := make([]KT, 0)
+	preUnHitKeys := make([]string, 0)
 	for i, bs := range cacheResult {
 		if len(bs) == 0 {
 			unHitKeys = append(unHitKeys, keys[i])
+			preUnHitKeys = append(preUnHitKeys, preKeys[i])
 			continue
 		}
 		if bytes.Compare(bs, c.emptyCacheVal) == 0 {
 			continue
 		}
 		temp := new(VT)
-		if err := c.serializer.Unmarshal(bs, temp); err != nil {
-			return nil, nil, err
+		if err = c.serializer.Unmarshal(bs, temp); err != nil {
+			return
 		}
 		result[keys[i]] = *temp
 	}
-	return result, unHitKeys, nil
+	if len(unHitKeys) == 0 {
+		return
+	}
+
+	var getterResult map[KT]VT
+	if getterResult, err = c.getterSetCache(ctx, unHitKeys, preUnHitKeys, getter, opts...); err == nil {
+		for k, v := range getterResult {
+			result[k] = v
+		}
+	}
+	return
 }
 
-func (c *SimpleCache[KT, VT]) getterSetCache(ctx context.Context, keys []KT, fn GetterFunc[KT, VT]) (err error) {
-	if len(keys) == 0 {
+func (c *SimpleCache[KT, VT]) getterSetCache(ctx context.Context, unHitKeys []KT, preUnHitKeys []string, getter MGetterFunc[KT, VT], opts ...interface{}) (result map[KT]VT, err error) {
+	var getterResult map[KT]VT
+	if getterResult, err = getter(ctx, unHitKeys, opts...); err != nil {
 		return
 	}
-	getterResult, err := fn(ctx, keys)
-	if err != nil {
-		return
-	}
-	emptyKeys := make([]string, 0, len(getterResult))
+
+	result = make(map[KT]VT)
 	kvs := make([]interface{}, 0, len(getterResult)*2)
-	preKeys, err := c.policy.Pre(keys)
-	if err != nil {
-		return err
-	}
-	for i, key := range keys {
-		preKey := preKeys[i]
-		data, ok := getterResult[key]
-		if !ok {
-			emptyKeys = append(emptyKeys, preKey)
-			continue
-		}
+	for key, data := range getterResult {
 		var bs []byte
 		if bs, err = c.serializer.Marshal(data); err != nil {
 			return
 		}
-		kvs = append(kvs, preKey, bs)
+		kvs = append(kvs, c.fmtKeyFn([]KT{key}, opts...)[0], bs)
+		result[key] = data
 	}
-	if err = c.policy.OnCross(emptyKeys); err != nil {
-		return err
+
+	for i, key := range unHitKeys {
+		if _, ok := getterResult[key]; ok {
+			continue
+		}
+		kvs = append(kvs, preUnHitKeys[i], c.emptyCacheVal)
 	}
+
 	if err = c.operator.MSet(kvs...); err != nil {
-		return err
+		return
 	}
 	return
 }
